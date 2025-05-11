133996fac61553bc5d31a6775e505f6c62ae99d2
itaycohen <itay@trekace.com>
2025-05-11 08:50:12 +0300

    loggers_android/disklogger 1.8

--- loggers_android/build.gradle.kts
+++ loggers_android/build.gradle.kts
@@ -53,7 +53,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.itayc"
             artifactId = "iclogger-android-loggers"
-            version = "1.7"
+            version = "1.8"
             afterEvaluate {
                 from(components["release"])
             }
